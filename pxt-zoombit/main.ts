fad3a44505bd04877436dd03b20ad250029abb78
kenken64 <bunnyppl@hotmail.com>
2025-09-11 03:03:08 +0800

    update main.ts

--- pxt-zoombit/main.ts
+++ pxt-zoombit/main.ts
@@ -1,22 +1,14 @@
-function setup() {
-  // Initialize the zoombit
-  zoombit.init();
-}
+// Auto-generated headlight control for: light up led when A is pressed then when B is pressed off the led
+input.onButtonPressed(Button.A, function () {
+  zoombit.setHeadlight(
+    HeadlightChannel.All,
+    zoombit.digitalStatePicker(DigitalIoState.On),
+  );
+});
 
-function loop() {
-  // Auto-generated headlight control for: turn on headlight when A is pressed
-  if (input.buttonIsPressed(Button.A)) {
-    zoombit.setHeadlight(
-      zoombit.HeadlightChannel.All,
-      zoombit.digitalStatePicker(zoombit.DigitalIoState.On),
-    );
-  } else {
-    zoombit.setHeadlight(
-      zoombit.HeadlightChannel.All,
-      zoombit.digitalStatePicker(zoombit.DigitalIoState.Off),
-    );
-  }
-}
-
-setup();
-basic.forever(() => loop());
+input.onButtonPressed(Button.B, function () {
+  zoombit.setHeadlight(
+    HeadlightChannel.All,
+    zoombit.digitalStatePicker(DigitalIoState.Off),
+  );
+});
